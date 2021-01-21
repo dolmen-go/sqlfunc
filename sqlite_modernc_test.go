97e6e3e426c2916951a74e67d6c624cf8b364835
Olivier Mengué <dolmen@cpan.org>
2021-01-21 21:48:59 +0100

    tests: allow to disable use of modernc

--- sqlite_modernc_test.go
+++ sqlite_modernc_test.go
@@ -1,5 +1,9 @@
 // Platforms where modernc.org/sqlite is supported:
 // +build linux,386 linux,amd64 linux,arm linux,arm64 darwin,amd64
+//
+// Allow to disable use of modernc.org/sqlite:
+//    go test -v -count=1 -tags nomodernc
+// +build !nomodernc
 
 package sqlfunc_test
 
