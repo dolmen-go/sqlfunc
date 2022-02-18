e5e04c0b6f3ef3f70bc0559becb7242728d0c5de
Olivier Mengué <dolmen@cpan.org>
2022-02-18 23:42:50 +0100

    Add TestSQLiteVersion
    Show SQLite version used for tests:
    - go test -v -run TestSQLiteVersion
    - go test -v -tags nomodernc -run TestSQLiteVersion

--- sqlite_test.go
+++ sqlite_test.go
@@ -1,5 +1,35 @@
 package sqlfunc_test
 
-import _ "github.com/mattn/go-sqlite3"
+import (
+	"context"
+	"database/sql"
+	"reflect"
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 var sqliteDriver = "sqlite3"
+
+// go test -v -run TestSQLiteVersion
+// go test -v -run TestSQLiteVersion -tags nomodernc
+func TestSQLiteVersion(t *testing.T) {
+	ctx := context.Background()
+	db, err := sql.Open(sqliteDriver, ":memory:")
+	if err != nil {
+		t.Logf("Open: %v", err)
+		return
+	}
+	defer db.Close()
+
+	var version string
+	if err = db.QueryRowContext(ctx, `SELECT sqlite_version()`).Scan(&version); err != nil {
+		t.Logf("sqlite_version(): %v", err)
+		return
+	}
+
+	t.Logf(
+		"%s (driver: %s, package %s)",
+		version, sqliteDriver, reflect.TypeOf(db.Driver()).Elem().PkgPath(),
+	)
+}
