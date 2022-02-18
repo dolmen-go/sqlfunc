fcdaee924a444fc345d5bff6667267ecf5f6f758
Olivier Mengué <dolmen@cpan.org>
2022-02-18 23:23:36 +0100

    tests: update build flags for modernc.org/sqlite

--- sqlite_modernc_test.go
+++ sqlite_modernc_test.go
@@ -1,9 +1,10 @@
-// Platforms where modernc.org/sqlite is supported:
-// +build linux,386 linux,amd64 linux,arm linux,arm64 darwin,amd64
-//
 // Allow to disable use of modernc.org/sqlite:
 //    go test -v -count=1 -tags nomodernc
+// Enable only on platforms where modernc.org/sqlite is officially supported:
+//
+//go:build !nomodernc && ((linux && 386) || (linux && amd64) || (linux && arm) || (linux && arm64) || (darwin && amd64) || (darwin && arm64) || (freebsd && amd64) || (windows && amd64))
 // +build !nomodernc
+// +build linux,386 linux,amd64 linux,arm linux,arm64 darwin,amd64 darwin,arm64 freebsd,amd64 windows,amd64
 
 package sqlfunc_test
 
