5df2d0c0e54b565e5588dfff65771d28be631101
Olivier Mengué <dolmen@cpan.org>
2022-04-21 23:19:42 +0200

    TestScanSrc: generate code for ForEach (WiP)
    Produce source code for ForEach functions.
    
    Still much more work to do:
    - check that the function code doesn't reference named types defined out
      of the package scope
    - collect referenced packages to produce imports

--- poc_test.go
+++ poc_test.go
@@ -1,10 +1,15 @@
 package sqlfunc_test
 
 import (
+	"bytes"
+	"errors"
 	"go/ast"
 	"go/token"
 	"go/types"
+	"strconv"
+	"strings"
 	"testing"
+	"text/template"
 
 	"golang.org/x/tools/go/ast/astutil"
 	"golang.org/x/tools/go/packages"
@@ -82,7 +87,9 @@ func TestScanSrc(t *testing.T) {
 					// Function expected:
 					// - literal
 					// - identifier pointing to a func type
-					genForEach(t, pkg, ti.TypeOf(arg).(*types.Signature))
+					if err := genForEach(t, pkg, ti.TypeOf(arg).(*types.Signature)); err != nil {
+						t.Logf("%s %v", pkg.Fset.Position(c.Pos()), err)
+					}
 					// As the argument might be a func literal, we want to go deeper in the AST
 					return true
 				default:
@@ -131,6 +138,90 @@ func gen(tb testing.TB, pkg *packages.Package, f string, sig *types.Signature) {
 	tb.Log(pkg.Name, f, sig)
 }
 
-func genForEach(tb testing.TB, pkg *packages.Package, sig *types.Signature) {
+func genForEach(tb testing.TB, pkg *packages.Package, sig *types.Signature) error {
 	tb.Log(pkg.Name, "ForEach", sig)
+
+	if sig.Params().Len() == 0 {
+		return errors.New("function must receive at least one parameter")
+	}
+
+	// FIXME We should check if all types of args are available at the package scope (not types defined locally in a function)
+	// and skip because we will not be able to generate code that reference those types
+
+	var withError bool
+	switch sig.Results().Len() {
+	case 0:
+	case 1:
+		if sig.Results().At(0).Type().String() == "error" {
+			withError = true
+			break
+		}
+		fallthrough
+	default:
+		return errors.New("only one return value allowed of type error")
+	}
+
+	params := sig.Params()
+	nParams := params.Len()
+	vars := make([]string, nParams)
+	args := make([]string, nParams)
+	for i := range nParams {
+		p := params.At(i)
+		name := "v" + strconv.Itoa(i)
+		// TODO collect reference to an import in p.Type
+		vars[i] = name + " " + p.Type().String()
+		args[i] = name
+	}
+
+	data := map[string]any{
+		"Type":      sig.String(),
+		"WithError": withError,
+		"Vars":      strings.Join(vars, "\n\t\t\t"),
+		"Args":      strings.Join(args, ", "),
+		"ArgsPtr":   "&" + strings.Join(args, ", &"),
+	}
+
+	const code = `` +
+		`sqlfunc.Ř.ForEach.Register(({{.Type}})(nil), func(rows *sql.Rows, cb interface{}) (err error) {
+	cb := cb.({{.Type}})
+	defer func() {
+		err2 := rows.Close()
+		if err == nil {
+			err = err2
+		}
+	}()
+	for rows.Next() {
+		var (
+			{{.Vars}}
+		)
+		if err = rows.Scan({{.ArgsPtr}}); err != nil {
+			return
+		}
+{{- if .WithError}}
+		if err = cb({{.Args}}); err != nil {
+			return
+		}
+{{- else}}
+		cb({{.Args}})
+{{- end}}
+	}
+	err = rows.Err()
+	return
+})
+`
+
+	tmpl := template.New("code")
+	tmpl, err := tmpl.Parse(code)
+	if err != nil {
+		panic(err)
+	}
+
+	var buf bytes.Buffer
+	if err = tmpl.Execute(&buf, data); err != nil {
+		panic(err)
+	}
+
+	tb.Log("\n" + buf.String())
+
+	return nil
 }
